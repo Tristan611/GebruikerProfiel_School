452c9f3662f636c6a0f4d36fa3dba0fba1d8d8c1
Tristan Jansen <s1169062@student.windesheim.nl>
2023-02-17 13:05:21 +0100

    Add project files.

---
+++ GebruikerProfiel/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Security.Policy;
+using GebruikerProfiel.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GebruikerProfiel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+		private const string PageViews = "PageViews";
+        public HomeController(ILogger<HomeController> logger)
+        {
+			_logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            UpdatePageViewCookie();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+			UpdatePageViewCookie();
+			return View();
+        }
+
+        public IActionResult Contact()
+        {
+            UpdatePageViewCookie();
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+		public void UpdatePageViewCookie()
+		{
+			var currentCookieValue = Request.Cookies[PageViews];
+
+			if (currentCookieValue == null)
+			{
+				Response.Cookies.Append(PageViews, "1");
+			}
+			else
+			{
+				var newCookieValue = short.Parse(currentCookieValue) + 1;
+
+				Response.Cookies.Append(PageViews, newCookieValue.ToString());
+                
+			}
+		}
+	}
+}
---
+++ GebruikerProfiel/wwwroot/js/site.js
@@ -0,0 +1,59 @@
+﻿class GDPR {
+
+    constructor() {
+        
+        this.bindEvents();
+        if (this.cookieStatus() == 'accept' || this.cookieStatus() == 'reject') {
+            this.hideGDPR();
+        } else {
+            this.showGDPR
+        }
+
+    }
+
+    bindEvents() {
+        let buttonAccept = document.querySelector('.button-accept');
+        buttonAccept.addEventListener('click', () => {
+            console.log("accept click");
+            this.cookieStatus('accept');
+            this.hideGDPR();
+        });
+
+
+        //student uitwerking
+        let buttonReject = document.querySelector('.button-reject');
+        buttonReject.addEventListener('click', () => {
+            console.log("reject click");
+            this.cookieStatus('reject');
+            this.hideGDPR();
+        });
+    }
+
+    
+
+    cookieStatus(status) {
+        console.log(status);
+        if (status == 'accept') {
+
+            localStorage.setItem('gdpr-consent-choice', status);
+        } else {
+            if (status == 'reject') localStorage.setItem('gdpr-consent-choice', status);
+        }
+
+        //student uitwerking
+
+        return localStorage.getItem('gdpr-consent-choice');
+    }
+
+    hideGDPR() {
+        document.querySelector(`.cookies`).classList.add('hide');
+        document.querySelector(`.cookies`).classList.remove('show');
+    }
+        
+    showGDPR() {
+        document.querySelector(`.cookies`).classList.add('show');
+    }   
+}
+const gdpr = new GDPR();
+
+
